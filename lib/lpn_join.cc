520a60c6414d68e2d09964e26add5b9d261c1fdf
Minwoo Jeong <jeongmwj@gmail.com>
2023-05-20 21:43:20 +0900

    Update `Print` method

--- lib/lpn_join.cc
+++ lib/lpn_join.cc
@@ -1,5 +1,6 @@
 #include "lpn_join.h"
 
+#include <iomanip>
 #include <iostream>
 
 LpnJoin::LpnJoin() = default;
@@ -16,8 +17,10 @@ std::shared_ptr<Lpn> LpnJoin::GetReplica() {
 LpnInfo LpnJoin::what() { return LpnInfo{"LpnJoin", this}; }
 void LpnJoin::Print() {
   std::cout << "Root: " << this->what().ToString() << std::endl;
-  std::cout << "Left: " << this->left_->what().ToString() << std::endl;
-  std::cout << "Right: " << this->right_->what().ToString() << std::endl;
+  std::cout << "(Left)" << std::endl;
+  this->left_->Print();
+  std::cout << "(Right)" << std::endl;
+  this->right_->Print();
 }
 
 std::shared_ptr<Lpn> LpnJoin::left() const { return left_; }
