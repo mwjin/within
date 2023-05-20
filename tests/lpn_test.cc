0f1ece57f1eb59a86e61089200981a7fcf6817ac
Minwoo Jeong <jeongmwj@gmail.com>
2023-05-20 14:30:13 +0900

    Add a test `ToogleLpnBuffSwitch`

--- tests/lpn_test.cc
+++ tests/lpn_test.cc
@@ -2,6 +2,7 @@
 #include <gtest/gtest.h>
 
 #include "lpn_buff.h"
+#include "lpn_buff_switch.h"
 #include "lpn_proj.h"
 
 namespace lpn {
@@ -16,6 +17,16 @@ TEST(LpnTest, ReplicateLpnProj) {
   auto lpn{std::make_unique<LpnProj>()};
   auto replica{lpn->GetReplica()};
   EXPECT_NE(lpn.get(), replica.get());
+}
+
+TEST(LpnTest, ToggleLpnBuffSwitch) {
+  auto buff_switch{std::make_unique<LpnBuffSwitch>()};
+  auto before_toggle{buff_switch->GetReplica()};
+
+  buff_switch->Toggle();
+  auto after_toggle{buff_switch->GetReplica()};
+
+  EXPECT_NE(before_toggle->what().ToString(), after_toggle->what().ToString());
 }
 }  // namespace
 }  // namespace lpn
